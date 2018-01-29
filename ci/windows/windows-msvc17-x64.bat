91b349940c1da5eedefe55f4bd3e54fde9966e1f
Martin Höher <martin@rpdev.net>
2018-01-29 22:19:19 +0100

    Fix in Win MSVC Build Scripts

--- ci/windows/windows-msvc17-x64.bat
+++ ci/windows/windows-msvc17-x64.bat
@@ -1,10 +1,8 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
 set PATH=C:\Qt\Tools\QtCreator\bin;%PATH%
 set PATH=C:\Qt\5.10.0\msvc2017_64\bin;%PATH%
-set PATH="C:\Program Files (x86)\NSIS;%PATH%"
 mkdir build-win64-vs
 cd build-win64-vs
-echo %PATH%
 "C:\Program Files\CMake\bin\cmake.exe" ^
     -G"NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE=Release ^
@@ -12,12 +10,12 @@ echo %PATH%
     -DUSE_CREDENTIAL_STORE=ON ^
     -DQTKEYCHAIN_STATIC=ON ^
     ..
-jom -j4
+nmake
 mkdir bin
 copy app\release\OpenTodoList.exe bin
 copy ..\pre-build\windows\openssl-1.0.2l-x64_86-win64\ssleay32.dll bin\ssleay32.dll
 copy ..\pre-build\windows\openssl-1.0.2l-x64_86-win64\libeay32.dll bin\libeay32.dll
 windeployqt --qmldir ..\app --release --compiler-runtime bin\OpenTodoList.exe
 copy ..\templates\nsis\win64-installer.nsis .
-makensis win64-installer.nsis
+"C:\Program Files (x86)\NSIS\makensis.exe" win64-installer.nsis
 copy OpenTodoList-Windows-64bit.exe OpenTodoList-Windows-64bit-MSVC.exe
