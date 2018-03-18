6ab45e0bed34085f712987327ebc10ba6e16b705
Martin Höher <martin@roberts-mbp.localdomain>
2018-03-18 16:22:16 +0100

    Imrpove macOS CI Script
    We now take the Qt installation directory from the QT_DIR environment
    variable. This makes the script more independent from the device a CI
    job is executed on.

--- ci/macos-build.sh
+++ ci/macos-build.sh
@@ -2,7 +2,7 @@
 mkdir build-macos
 cd build-macos
 
-QTSDK=/Users/mhoeher/Qt/5.10.0/clang_64/
+QTSDK=$QT_DIR/5.10.1/clang_64/
 
 /Applications/CMake.app/Contents/bin/cmake \
     -DCMAKE_PREFIX_PATH=$QTSDK \
