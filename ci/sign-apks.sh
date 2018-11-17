ffe6fbb1831584971c42fde446b5de3956ab4a51
Martin Höher <martin@rpdev.net>
2018-11-17 19:26:18 +0100

    Sign APKs in CI

---
+++ ci/sign-apks.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+cd build-android-armv7
+jarsigner \
+    -sigalg SHA1withRSA \
+    -digestalg SHA1 \
+    -keystore "$OPENTODOLIST_KEYSTORE" \
+    -storepass "$OPENTODOLIST_KEYSTORE_SECRET" \
+    OpenTodoList-Android-armv7.apk mhoeher
+$ANDROID_SDK_ROOT/build-tools/27.0.3/zipalign \
+    -v 4 \
+    OpenTodoList-Android-armv7.apk \
+    OpenTodoList-Android-armv7-aligned.apk
+cd ..
+
+cd build-android-x86/
+jarsigner \
+    -sigalg SHA1withRSA \
+    -digestalg SHA1 \
+    -keystore "$OPENTODOLIST_KEYSTORE" \
+    -storepass "$OPENTODOLIST_KEYSTORE_SECRET" \
+    OpenTodoList-Android-x86.apk mhoeher
+$ANDROID_SDK_ROOT/build-tools/27.0.3/zipalign \
+    -v 4 \
+    OpenTodoList-Android-x86.apk \
+    OpenTodoList-Android-x86-aligned.apk
+cd ..
