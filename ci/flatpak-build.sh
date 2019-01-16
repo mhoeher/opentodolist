92928104b48617e2d7d1569ba895bbe8393b6f0a
Martin Höher <martin@rpdev.net>
2019-01-16 19:50:17 +0100

    Sign Flatpak repository in CI

--- ci/flatpak-build.sh
+++ ci/flatpak-build.sh
@@ -7,6 +7,9 @@ if [ -n "$CI" ]; then
         flatpak \
         flatpak-builder \
         libappstream-glib
+    echo "$FLATPAK_SIGNING_KEY" > flatpak-signing-key.asc
+    gpg2 --import ./flatpak-signing-key.asc
+    rm flatpak-signing-key.asc
 fi
 
 appstream-util validate-relax net.rpdev.OpenTodoList.appdata.xml
@@ -22,5 +25,6 @@ flatpak-builder \
     --ccache \
     --repo=fp-repo \
     --subject="Build of OpenTodoList `date`" \
+    --gpg-sign 3D5B6355C13D18AB \
     fp-app \
     net.rpdev.OpenTodoList.json
