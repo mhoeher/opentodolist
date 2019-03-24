0a95ff2bccd2b60edb976d2be7b9408ece8b9e12
Martin Höher <martin@rpdev.net>
2019-03-24 16:07:28 +0100

    Integrate syntax highlighting in macOS build
    Build and deploy the syntax highlighting library in the macOS build.

--- ci/macos-build.sh
+++ ci/macos-build.sh
@@ -2,9 +2,10 @@
 
 set -e
 
-rm -rf build-macos
-mkdir build-macos
-cd build-macos
+BUILD_DIR=$PWD/build-macos
+PACKAGES_DIR=$BUILD_DIR/__packages
+
+rm -rf $BUILD_DIR
 
 if [ ! -d "$QT_DIR" ]; then
     echo "The variable QT_DIR is not set"
@@ -12,17 +13,46 @@ if [ ! -d "$QT_DIR" ]; then
 fi
 
 QTSDK=$QT_DIR
+CMAKE=/Applications/CMake.app/Contents/bin/cmake
 
-/Applications/CMake.app/Contents/bin/cmake \
-    -DCMAKE_PREFIX_PATH=$QTSDK \
+if [ -n "$CI" ]; then
+    # Install ECM:
+    pushd 3rdparty/KDE/extra-cmake-modules
+    mkdir -p build-macos
+    cd build-macos
+    $CMAKE -DCMAKE_PREFIX_PATH=$QTSDK \
+        -DCMAKE_INSTALL_PREFIX=$PACKAGES_DIR \
+        ..
+    $CMAKE --build .
+    $CMAKE --build . --target install
+    popd
+
+    # Install KDE syntax highlighting
+    pushd 3rdparty/KDE/syntax-highlighting/
+    mkdir -p build-macos
+    cd build-macos
+    $CMAKE -DCMAKE_PREFIX_PATH=$QTSDK \
+        -DCMAKE_INSTALL_PREFIX=$PACKAGES_DIR \
+        -Dt5_DIR=$QTSDK/lib/cmake \
+        ..
+    $CMAKE --build .
+    $CMAKE --build . --target install
+    popd
+fi
+
+mkdir -p $BUILD_DIR
+cd $BUILD_DIR
+
+$CMAKE \
+    -DCMAKE_PREFIX_PATH=$QTSDK\;$PACKAGES_DIR \
     -DCMAKE_BUILD_TYPE=Release \
     -DOPENTODOLIST_WITH_UPDATE_SERVICE=ON \
     -DQTKEYCHAIN_STATIC=ON \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
     ..
-make opentodolist-translations
-make -j2
-make test
+$CMAKE --build . --target opentodolist-translations
+$CMAKE --build . -- -j2
+$CMAKE --build . -- test
 $QTSDK/bin/macdeployqt app/OpenTodoList.app/ -qmldir=../app
 
 # Prepare a "beautified" folder:
