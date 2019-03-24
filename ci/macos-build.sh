71262fd05a5d1e202739ceff9e69af582728fbc4
Martin Höher <martin@rpdev.net>
2019-03-24 16:23:47 +0100

    Use Ninja in macOS build

--- ci/macos-build.sh
+++ ci/macos-build.sh
@@ -13,46 +13,50 @@ if [ ! -d "$QT_DIR" ]; then
 fi
 
 QTSDK=$QT_DIR
-CMAKE=/Applications/CMake.app/Contents/bin/cmake
 
 if [ -n "$CI" ]; then
     # Install ECM:
     pushd 3rdparty/KDE/extra-cmake-modules
     mkdir -p build-macos
     cd build-macos
-    $CMAKE -DCMAKE_PREFIX_PATH=$QTSDK \
+    cmake \
+        -GNinja \
+        -DCMAKE_PREFIX_PATH=$QTSDK \
         -DCMAKE_INSTALL_PREFIX=$PACKAGES_DIR \
         ..
-    $CMAKE --build .
-    $CMAKE --build . --target install
+    cmake --build .
+    cmake --build . --target install
     popd
 
     # Install KDE syntax highlighting
     pushd 3rdparty/KDE/syntax-highlighting/
     mkdir -p build-macos
     cd build-macos
-    $CMAKE -DCMAKE_PREFIX_PATH=$QTSDK \
+    cmake \
+        -GNinja \
+        -DCMAKE_PREFIX_PATH=$QTSDK \
         -DCMAKE_INSTALL_PREFIX=$PACKAGES_DIR \
         -Dt5_DIR=$QTSDK/lib/cmake \
         ..
-    $CMAKE --build .
-    $CMAKE --build . --target install
+    cmake --build .
+    cmake --build . --target install
     popd
 fi
 
 mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
-$CMAKE \
+cmake \
+    -GNinja \
     -DCMAKE_PREFIX_PATH=$QTSDK\;$PACKAGES_DIR \
     -DCMAKE_BUILD_TYPE=Release \
     -DOPENTODOLIST_WITH_UPDATE_SERVICE=ON \
     -DQTKEYCHAIN_STATIC=ON \
     -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
     ..
-$CMAKE --build . --target opentodolist-translations
-$CMAKE --build . -- -j2
-$CMAKE --build . -- test
+cmake --build . --target opentodolist-translations
+cmake --build . -- -j2
+cmake --build . -- test
 $QTSDK/bin/macdeployqt app/OpenTodoList.app/ -qmldir=../app
 
 # Prepare a "beautified" folder:
