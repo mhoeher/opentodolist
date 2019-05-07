f1da3c9f8523360583213242d808decb0547cdbd
Martin Höher <martin@rpdev.net>
2019-05-07 21:32:02 +0200

    Use new qt-cmake-android module for Android build
    Integrated new qt-cmake-android module. It provides a way to build for
    Android both from within Qt Creator as well as on command line.

--- 3rdparty/qtkeychain/androidkeystore_p.h
+++ 3rdparty/qtkeychain/androidkeystore_p.h
@@ -211,7 +211,7 @@ public:
     class PrivateKeyEntry : public Entry
     {
     public:
-        using Object::Object;
+        using Entry::Entry;
 
         inline PrivateKeyEntry(const Entry &init): Entry(init) {}
 
