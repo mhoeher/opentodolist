e9c42bdfde47bc0f3e9a47691edb406574f9930f
Martin Höher <martin@rpdev.net>
2019-07-30 22:37:40 +0200

    Android build: Also sign 64bit x86 APK

--- ci/sign-apks.sh
+++ ci/sign-apks.sh
@@ -40,3 +40,17 @@ $ANDROID_SDK_ROOT/build-tools/*/zipalign \
     OpenTodoList-Android-x86.apk \
     OpenTodoList-Android-x86-aligned.apk
 cd ..
+
+
+cd build-android-x86_64/
+jarsigner \
+    -sigalg SHA1withRSA \
+    -digestalg SHA1 \
+    -keystore "$OPENTODOLIST_KEYSTORE" \
+    -storepass "$OPENTODOLIST_KEYSTORE_SECRET" \
+    OpenTodoList-Android-x86.apk "$OPENTODOLIST_KEYSTORE_ALIAS"
+$ANDROID_SDK_ROOT/build-tools/*/zipalign \
+    -v 4 \
+    OpenTodoList-Android-x86_64.apk \
+    OpenTodoList-Android-x86_64-aligned.apk
+cd ..
