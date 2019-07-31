231f09b18b4d137df4aac521d839cf3836fb1a78
Martin Höher <martin@rpdev.net>
2019-07-31 06:20:42 +0200

    Fix APK signing

--- ci/sign-apks.sh
+++ ci/sign-apks.sh
@@ -2,55 +2,17 @@
 
 set -e
 
-cd build-android-armv7
-jarsigner \
-    -sigalg SHA1withRSA \
-    -digestalg SHA1 \
-    -keystore "$OPENTODOLIST_KEYSTORE" \
-    -storepass "$OPENTODOLIST_KEYSTORE_SECRET" \
-    OpenTodoList-Android-armv7.apk "$OPENTODOLIST_KEYSTORE_ALIAS"
-$ANDROID_SDK_ROOT/build-tools/*/zipalign \
-    -v 4 \
-    OpenTodoList-Android-armv7.apk \
-    OpenTodoList-Android-armv7-aligned.apk
-cd ..
-
-cd build-android-arm64_v8a/
-jarsigner \
-    -sigalg SHA1withRSA \
-    -digestalg SHA1 \
-    -keystore "$OPENTODOLIST_KEYSTORE" \
-    -storepass "$OPENTODOLIST_KEYSTORE_SECRET" \
-    OpenTodoList-Android-arm64_v8a.apk "$OPENTODOLIST_KEYSTORE_ALIAS"
-$ANDROID_SDK_ROOT/build-tools/*/zipalign \
-    -v 4 \
-    OpenTodoList-Android-arm64_v8a.apk \
-    OpenTodoList-Android-arm64_v8a-aligned.apk
-cd ..
-
-cd build-android-x86/
-jarsigner \
-    -sigalg SHA1withRSA \
-    -digestalg SHA1 \
-    -keystore "$OPENTODOLIST_KEYSTORE" \
-    -storepass "$OPENTODOLIST_KEYSTORE_SECRET" \
-    OpenTodoList-Android-x86.apk "$OPENTODOLIST_KEYSTORE_ALIAS"
-$ANDROID_SDK_ROOT/build-tools/*/zipalign \
-    -v 4 \
-    OpenTodoList-Android-x86.apk \
-    OpenTodoList-Android-x86-aligned.apk
-cd ..
-
-
-cd build-android-x86_64/
-jarsigner \
-    -sigalg SHA1withRSA \
-    -digestalg SHA1 \
-    -keystore "$OPENTODOLIST_KEYSTORE" \
-    -storepass "$OPENTODOLIST_KEYSTORE_SECRET" \
-    OpenTodoList-Android-x86.apk "$OPENTODOLIST_KEYSTORE_ALIAS"
-$ANDROID_SDK_ROOT/build-tools/*/zipalign \
-    -v 4 \
-    OpenTodoList-Android-x86_64.apk \
-    OpenTodoList-Android-x86_64-aligned.apk
-cd ..
+for arch in armv7 arm64_v8a x86 x86_64; do
+    pushd build-android-$arch
+    jarsigner \
+        -sigalg SHA1withRSA \
+        -digestalg SHA1 \
+        -keystore "$OPENTODOLIST_KEYSTORE" \
+        -storepass "$OPENTODOLIST_KEYSTORE_SECRET" \
+        OpenTodoList-Android-${arch}.apk "$OPENTODOLIST_KEYSTORE_ALIAS"
+    $ANDROID_SDK_ROOT/build-tools/*/zipalign \
+        -v 4 \
+        OpenTodoList-Android-${arch}.apk \
+        OpenTodoList-Android-${arch}-aligned.apk
+    popd
+done
