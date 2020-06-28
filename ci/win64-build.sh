8bb636742b8624eb2e5c72a66b5ba63a2d90d836
Martin Höher <martin@rpdev.net>
2020-06-28 18:12:19 +0200

    Fix x-Win ccache integration

--- ci/win64-build.sh
+++ ci/win64-build.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Enable ccache:
-ln -s "$(which x86_64-w64-mingw32-g++)" /usr/local/bin/x86_64-w64-mingw32-g++
-ln -s "$(which x86_64-w64-mingw32-gcc)" /usr/local/bin/x86_64-w64-mingw32-gcc
+ln -s /usr/bin/ccache /usr/local/bin/x86_64-w64-mingw32-g++
+ln -s /usr/bin/ccache /usr/local/bin/x86_64-w64-mingw32-gcc
 
 TARGET=win64 ./ci/win-build.sh
