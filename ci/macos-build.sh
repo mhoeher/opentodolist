8ae0a39856a07a1db60b54568fcfa5dea4ed3a4c
Martin Höher <martin@rpdev.net>
2020-07-18 19:10:46 +0200

    Notarize macOS app
    Notarize the app on macOS. In addition, try to upload it (currently
    fails for unknown reasons...)
    
    Also, try to re-run DMG creation if it fails on the first attempt.
    
    Closes #333

--- ci/macos-build.sh
+++ ci/macos-build.sh
@@ -25,14 +25,68 @@ $QTSDK/bin/qmake \
     ..
 make -j4
 make check
+
+# Include Qt Runtime in App Bundle. Also sign the bundle
+# and prepare it for notarization:
 $QTSDK/bin/macdeployqt \
     app/OpenTodoList.app/ \
     -qmldir=../app \
     -appstore-compliant \
-    -codesign="Apple Distribution: Martin Hoeher (786Z636JV9)"
+    -sign-for-notarization="Developer ID Application: Martin Hoeher (786Z636JV9)"
+
+# Create a zip archive suitable for uploading to the notarization
+# service:
+ditto -ck --rsrc --sequesterRsrc "app/OpenTodoList.app" "app/OpenTodoList.zip"
 
 # Make sure the app has been signed:
 codesign -v app/OpenTodoList.app
+
+# Upload the archive for notarization:
+REQUEST_UUID=$(xcrun altool \
+    --notarize-app \
+    -t osx \
+    -f "app/OpenTodoList.zip" \
+    --primary-bundle-id="net.rpdev.opentodolist.zip" \
+    -u "martin@rpdev.net" \
+    -p "$OPENTODOLIST_STORE_KEY" | \
+    grep 'RequestUUID =' | awk '{ print $3; }')
+
+i="0"
+while [ $i -lt 120 ]; do
+    echo "Waiting for notarization to complete..."
+    sleep 30
+    STATUS=$(xcrun altool \
+        --notarization-info $REQUEST_UUID \
+        -u "martin@rpdev.net" \
+        -p "$OPENTODOLIST_STORE_KEY" | \
+        grep 'Status:' | awk '{ print $2 $3; }')
+    echo "Notarization status is: ${STATUS}"
+    case $STATUS in
+        success)
+            echo "Notarization finished successfully..."
+            break
+        ;;
+        "inprogress")
+            echo "Notarization still ongoing..."
+            continue
+        ;;
+        *)
+            echo "Notarization finished with an error!"
+            false
+        ;;
+    esac
+    i=$[$i+1]
+done
+
+# Include the notarization ticket in the app bundle:
+xcrun stapler staple "app/OpenTodoList.app"
+
+# Upload the App Bundle:
+xcrun altool \
+    --upload-app \
+    -f "app/OpenTodoList.app" \
+    -u "martin@rpdev.net" \
+    -p "$OPENTODOLIST_STORE_KEY" || true
 
 # Prepare a "beautified" folder:
 cd app
@@ -44,5 +98,17 @@ ln -s /Applications ./Applications
 cd ../
 
 # Create DMG file:
-hdiutil create -volname OpenTodoList -srcfolder ./dmg.in -ov -format UDZO OpenTodoList.dmg
-
+i="0"
+while [ $i -lt 5 ]; do
+    if ! hdiutil create \
+        -volname OpenTodoList \
+        -srcfolder ./dmg.in \
+        -ov -format UDZO \
+        OpenTodoList.dmg; then
+        echo "Creating disk image failed - retrying in 30s"
+        sleep 30
+    else
+        break
+    fi
+    i=$[$i+1]
+done
