a2ace8fcc7268e572bb66414ac3153b1acdf0d9f
Martin Höher <martin@rpdev.net>
2020-08-11 23:35:06 +0200

    CI build fixes for Arch Linux `makepkg`

--- ci/archlinux-pkgbuild.sh
+++ ci/archlinux-pkgbuild.sh
@@ -5,8 +5,6 @@ set -e
 PROJECT_ROOT=$(cd $(dirname $0) && cd .. && pwd)
 cd $PROJECT_ROOT
 
-pacman -Sy --noconfirm base-devel
-
 # Prepare build environment, see
 # http://allanmcrae.com/2015/01/replacing-makepkg-asroot/
 if [ ! -d /home/build ]; then
@@ -21,6 +19,14 @@ sed -i \
     -e "s/#commit=..pkgver./#commit=${CI_COMMIT_SHA}/g" \
     -e "s/release_build=yes/#release_build=yes/g" \
     /home/build/PKGBUILD
+
+# Note: We cannot use `makepkg -s`, as this requires a sudo password
+# which we in turn cannot enter during CI runs. Hence, extract dependencies
+# and install them before:
+pacman -Sy --noconfirm \
+    base-devel \
+    $( . PKGBUILD && echo ${depends[*]} ) \
+    $( . PKGBUILD && echo ${makedepends[*]} )
 (cd /home/build && sudo -u nobody makepkg)
 mkdir -p build-arch
 cp /home/build/OpenTodoList-*.pkg.tar.zst build-arch
