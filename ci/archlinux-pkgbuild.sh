7b772d6850f6baadf7b63a572f58116d8698a5da
Martin Höher <martin@rpdev.net>
2020-08-16 15:35:56 +0200

    Fixes in AUR PKGBUILD

--- ci/archlinux-pkgbuild.sh
+++ ci/archlinux-pkgbuild.sh
@@ -15,18 +15,24 @@ fi
 
 cp PKGBUILD /home/build/
 chown  nobody:nobody /home/build/PKGBUILD
-sed -i \
-    -e "s/#commit=..pkgver./#commit=${CI_COMMIT_SHA}/g" \
-    -e "s/release_build=yes/#release_build=yes/g" \
-    /home/build/PKGBUILD
+if [ -n "$CI_COMMIT_SHA" ]; then
+    sed -i \
+        -e "s/#commit=..pkgver./#commit=${CI_COMMIT_SHA}/g" \
+        -e "s/release_build=yes/#release_build=yes/g" \
+        /home/build/PKGBUILD
+fi
 
 # Note: We cannot use `makepkg -s`, as this requires a sudo password
 # which we in turn cannot enter during CI runs. Hence, extract dependencies
 # and install them before:
+mkdir -p build-arch
 pacman -Sy --noconfirm \
     base-devel \
     $( . PKGBUILD && echo ${depends[*]} ) \
     $( . PKGBUILD && echo ${makedepends[*]} )
-(cd /home/build && sudo -u nobody makepkg)
-mkdir -p build-arch
+(
+    cd /home/build && \
+    sudo -u nobody makepkg && \
+    sudo -u nobody makepkg --printsrcinfo > $PROJECT_ROOT/build-arch/.SRCINFO \
+)
 cp /home/build/opentodolist-*.pkg.tar.zst build-arch
