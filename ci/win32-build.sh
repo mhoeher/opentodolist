7fec746aeddffa5168b082c29e86e134fbeff2bd
Martin Hoeher <martin@rpdev.net>
2021-02-02 15:09:31 +0100

    Fix: Sync sometimes deletes files/folders
    This fixes two issues:
    
    - The file/folder filter was not correct.
    - There was an issue in SynqClient, so it sometimes constructed invalid
    paths (which in turn causes wrong decisions when calculating the sync
    trees/actions).

--- ci/win32-build.sh
+++ ci/win32-build.sh
@@ -3,8 +3,8 @@
 set -e
 
 # Enable ccache:
-ln -s /usr/bin/ccache /usr/local/bin/i686-w64-mingw32-g++
-ln -s /usr/bin/ccache /usr/local/bin/i686-w64-mingw32-gcc
+ln -s /usr/bin/ccache /usr/local/bin/i686-w64-mingw32-g++ || echo "Warning: Failed to enable ccache for g++"
+ln -s /usr/bin/ccache /usr/local/bin/i686-w64-mingw32-gcc || echo "Warning: Failed to enable ccache for gcc"
 export PATH=/usr/local/bin:$PATH
 
 TARGET=win32 ./ci/win-build.sh
