d20e8b2e28380aab8c933cd8aa8bca25677cdc96
Martin Hoeher <martin@rpdev.net>
2022-04-15 17:23:18 +0200

    Fix cpplint findings

--- test/test-utils.h
+++ test/test-utils.h
@@ -1,13 +1,32 @@
-#ifndef TESTUTILS_H
-#define TESTUTILS_H
+/*
+ * Copyright 2022 Martin Hoeher <martin@rpdev.net>
+ +
+ * This file is part of OpenTodoList.
+ *
+ * OpenTodoList is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * OpenTodoList is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OpenTodoList.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-#include <tuple>
+#ifndef TEST_UTILS_H_
+#define TEST_UTILS_H_
 
 #include <QString>
 #include <QUrl>
 #include <QVector>
 #include <QMap>
 #include <QTest>
+
+#include <tuple>
 
 #include "sync/webdavaccount.h"
 
@@ -89,4 +108,4 @@ inline void addWebDAVServerTestUrls()
     }
 }
 
-#endif // TESTUTILS_H
+#endif // TEST_UTILS_H_
