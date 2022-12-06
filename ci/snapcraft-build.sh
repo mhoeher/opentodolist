30a09cd2b2f3fda30121519ae6410cbbc807a94f
Martin Hoeher <martin@rpdev.net>
2022-12-06 23:05:45 +0100

    Fix: Include link to DRI drivers

--- ci/snapcraft-build.sh
+++ ci/snapcraft-build.sh
@@ -16,6 +16,9 @@ chmod +x OpenTodoList*.AppImage
 apt-get update && apt-get install -y rsync
 rsync -a ./squashfs-root/usr $SNAPCRAFT_PART_INSTALL
 
+# Create a symlink to DRI drivers are found:
+(cd $SNAPCRAFT_PART_INSTALL/usr/lib && ln -s *-linux-gnu/dri dri)
+
 # Patch desktop file entry:
 sed -i \
     -e 's#Icon=net.rpdev.OpenTodoList#Icon=\${SNAP}/usr/share/icons/hicolor/256x256/apps/net.rpdev.OpenTodoList.png#' \
